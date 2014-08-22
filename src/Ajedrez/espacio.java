7605fb28590dec988344a191b4bceb11a10fd111
chus55 <chusmurillo9@gmail.com>
2014-08-21 21:44:32 -0600

    Agregue clase espacio

---
+++ src/Ajedrez/espacio.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Ajedrez;
+
+/**
+ *
+ * @author Jesus
+ */
+public class espacio extends Pieza{
+
+    public espacio(int x, int y) {
+        super(x, y, "--");
+    }
+    
+     @Override
+    public void mover(int x, int y) {  
+    }
+}
